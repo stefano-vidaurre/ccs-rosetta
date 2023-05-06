bc787b4b0b39d6dec7fb85097834f5e806553b7f
Stefano Vidaurre Olite <fano494@gmail.com>
2023-05-06 15:06:48 +0200

    test: The project repository should return a list with inserted project

--- tests/CCS.Rosetta.Test/Projects/ProjectRepositoryShould.cs
+++ tests/CCS.Rosetta.Test/Projects/ProjectRepositoryShould.cs
@@ -1,11 +1,12 @@
 ﻿using System.Data.Common;
 using CCS.Rosetta.Api.Projects;
+using Dapper;
 using FluentAssertions;
 using Microsoft.Data.Sqlite;
 
 namespace CSS.Rosetta.Test.Projects;
 
-public class ProjectRepositoryShould
+public class ProjectRepositoryShould : IDisposable
 {
     private readonly DbConnection _connection;
     private readonly IProjectRepository _repository;
@@ -14,6 +15,13 @@ public class ProjectRepositoryShould
     {
         _connection = new SqliteConnection("Data Source=:memory:");
         _repository = new ProjectRepository(_connection);
+        _connection.Open();
+        _connection.Execute("CREATE TABLE IF NOT EXISTS Projects (Name VARCHAR(255) PRIMARY KEY, Description TEXT);");
+    }
+    
+    public void Dispose()
+    {
+        _connection.Dispose();
     }
 
     [Fact]
@@ -21,5 +29,15 @@ public class ProjectRepositoryShould
     {
         var result = await _repository.GetAll();
         result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task ReturnAListWithInsertedProject()
+    {
+        await _connection.ExecuteAsync("INSERT INTO Projects ('Name', 'Description') VALUES ('my-project', 'A description.');");
+        
+        IEnumerable<Project> result = await _repository.GetAll();
+        
+        result.Should().Contain(project => project.Name == "my-project" && project.Description == "A description.");
     }
 }
