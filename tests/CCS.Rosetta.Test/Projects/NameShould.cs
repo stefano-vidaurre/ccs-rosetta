955a387483c0245e5ffca7971b2963046615b570
Stefano Vidaurre Olite <fano494@gmail.com>
2023-05-18 18:23:24 +0200

    test: Name should not be constructed when value is null

---
+++ tests/CCS.Rosetta.Test/Projects/NameShould.cs
@@ -0,0 +1,15 @@
+﻿using CCS.Rosetta.Api.Projects;
+using FluentAssertions;
+
+namespace CSS.Rosetta.Test.Projects;
+
+public class NameShould
+{
+    [Fact]
+    public void NotBeConstructedWhenValueIsNull()
+    {
+        Func<Name> action = () => new Name(null);
+        
+        action.Should().Throw<ArgumentException>();
+    }
+}
