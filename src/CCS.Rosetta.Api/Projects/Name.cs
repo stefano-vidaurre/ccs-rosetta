fba79a7260e1d1cc08a7c78d47b915ad66c96658
Stefano Vidaurre Olite <fano494@gmail.com>
2023-05-18 18:24:53 +0200

    feat: Name constructor throws a exception when the value is null

--- src/CCS.Rosetta.Api/Projects/Name.cs
+++ src/CCS.Rosetta.Api/Projects/Name.cs
@@ -2,10 +2,15 @@
 
 public class Name
 {
+    public string Value { get; private set; }
+ 
     public Name(string name)
     {
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new ArgumentException("Value cannot be null or empty.", nameof(name));
+        }
+
         Value = name;
     }
-
-    public string Value { get; private set; }
 }
